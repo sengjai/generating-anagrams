b8b9492ea761502ffcaaa75151a421736239ba4d
Soon Seng <soonseng.ee@thetechyhub.com>
2016-09-07 10:12:52 +0800

    Generating Anagrams Done

--- generating-anagrams.rb
+++ generating-anagrams.rb
@@ -1,1 +1,32 @@
 # Place your solutions here
+def anagrams_for(string,dictionary)
+	list = []
+
+	dictionary.each do |word|
+		if canonical(word) == canonical(string)
+			list << word
+		end
+	end
+	return list.to_s
+end
+
+def canonical(word)
+	#Magic goes here
+	return word.downcase.split('').sort #downcase all letters, .split('') or .chars
+end
+
+
+
+
+
+dictionary = ['acres','cares','Cesar','races','smelt','melts','etlsm']
+
+puts anagrams_for('acres', dictionary)
+puts anagrams_for('ACRES', dictionary)
+puts anagrams_for('Cesar', dictionary)
+
+puts anagrams_for('sacre', dictionary)
+
+puts anagrams_for('etlsm', dictionary)
+
+puts anagrams_for('unicorn', dictionary)
